e22aa6d0e96d5ad02f51ec8dc186787ce1ec0a05
FatalMorgana73 <36102256+FatalMorgana73@users.noreply.github.com>
2021-03-22 18:23:58 +0100

    EVA - 21.03.2021

---
+++ src/de/nmauer/schule/Arrays/NZahlen.java
@@ -0,0 +1,49 @@
+package de.nmauer.schule.Arrays;
+
+public class NZahlen {
+
+    /*
+        EVA - 23.02.2021
+        Thema: Arrays
+     */
+
+    public int[] numbers = {1, 2, 3, 4, 5, 6 , 7, 8, 9, 0};
+
+    public NZahlen(){
+        // Main
+    }
+
+    public int getTotal(){
+
+        int total = 0;
+
+        for(int i: numbers){
+            total += i;
+        }
+
+        return total;
+    }
+
+    public int getSmallestNum(){
+        int smallest = numbers[0];
+
+        for(int i: numbers){
+            if (smallest > i)
+                smallest = i;
+        }
+
+        return smallest;
+    }
+
+    public int getAmountOfNum(int j){
+        int amount = 0;
+
+        for(int i: numbers){
+            if(i == j)
+                amount++;
+        }
+
+        return amount;
+    }
+
+}
