0b0080e18f49944dd2b1db187008837eecacc307
FatalMorgana73 <36102256+FatalMorgana73@users.noreply.github.com>
2021-03-23 13:24:30 +0100

    EVA - 21.03.2021

--- src/de/nmauer/schule/Arrays/NZahlen.java
+++ src/de/nmauer/schule/Arrays/NZahlen.java
@@ -10,7 +10,12 @@ public class NZahlen {
     public int[] numbers = {1, 2, 3, 4, 5, 6 , 7, 8, 9, 0};
 
     public NZahlen(){
-        // Main
+    }
+
+    public static void main(String[] args) {
+        NZahlen nZahlen = new NZahlen();
+
+        System.out.println(nZahlen.getTotal());
     }
 
     public int getTotal(){
